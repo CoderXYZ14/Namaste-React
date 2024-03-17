0700eb7f4e60cf5840560b12f1098956e6d7b6df
CoderXYZ14 <s1404islam@gmail.com>
2024-03-18 04:51:46 +0530

    L3

--- L2/app.js
+++ L2/app.js
@@ -1,6 +1,27 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-const heading = React.createElement("h1", {}, "Hello world");
+//React Element
+const heading1 = (
+  <h1 className="head" tabIndex="5">
+    Namaste React
+  </h1>
+);
+const Heading1 = () => (
+  <h1 className="head" tabIndex="5">
+    Namaste React
+  </h1>
+);
+
+//Raect Funcitonal Components
+const Heading = () => (
+  <div>
+    {/* both are same thibg */}
+    {Heading1()}
+    <Heading1 />
+    <h1>Namaste Raect</h1>
+  </div>
+);
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(heading);
+root.render(<Heading />);
