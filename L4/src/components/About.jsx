5628c0a58d14a7d008819efb4f8baf9e9b58884c
CoderXYZ14 <s1404islam@gmail.com>
2024-04-10 04:57:14 +0530

    L-8

--- L4/src/components/About.jsx
+++ L4/src/components/About.jsx
@@ -1,8 +1,13 @@
+import User from "./User";
+import UserClass from "./UserClass";
+
 const About = () => {
   return (
     <div>
       <h1>About</h1>
       <h2>This is Shahwaiz Islam</h2>
+      {/* <User name={"Shahwaiz"} /> */}
+      <UserClass name={"Shahwaiz"} />
     </div>
   );
 };
